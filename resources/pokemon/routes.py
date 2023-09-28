6f4a5166a211a115d8cdec56092e7eb0bb403721
Joshua Lim <joshuawlim96@gmail.com>
2023-09-28 16:25:40 -0700

    adjusted pokemon routes

--- resources/pokemon/routes.py
+++ resources/pokemon/routes.py
@@ -16,27 +16,29 @@ from db import pokemon
 class PokemonList(MethodView):
 
     # get all pokemon
-    
+  @jwt_required()
   @bp.response(200, PokemonSchema(many=True))
   def get_pokemon(self):
     return PokemonModel.query.all()
   
   # create pokemon
+  @jwt_required()
   @bp.arguments(PokemonSchema)
   @bp.response(200, PokemonSchema)
   def post(self, pokemon_data):
-      p = PokemonModel(**pokemon_data)
-      t = TrainerModel.query.get(pokemon_data['trainer_id'])
-      if t:
+      trainer_id = get_jwt_identity()
+      p = PokemonModel(**pokemon_data, trainer_id = trainer_id)
+      try:
           p.save()
           return p
-      else:
+      except IntegrityError:
           abort(400, message='Invalid Trainer ID')
 
 @bp.route('/<pokemon_id>')
 class Pokemon(MethodView):
 
     # get one pokemon
+  @jwt_required()
   @bp.response(200, PokemonSchema)
   def get(self, pokemon_id):
     p = PokemonModel.query.get(pokemon_id)
@@ -45,21 +47,25 @@ class Pokemon(MethodView):
     abort(400, message='Invalid Pokemon ID')
 
 # edit/evolve a pokemon
+  @jwt_required()
   @bp.arguments(PokemonSchema)
   @bp.response(200, PokemonSchema)
   def put(self, pokemon_data, pokemon_id):
     p = PokemonModel.query.get(pokemon_id)
     if p and pokemon_data['pokemon_species']:
-      if p.trainer_id == pokemon_data['trainer_id']:
+      trainer_id = get_jwt_identity()
+      if p.trainer_id == trainer_id:
         p.pokemon_species = pokemon_data['pokemon_species']
         p.save()
         return p
-      abort(400, message='Invalid Pokemon Data')
+      else:
+        abort(401, message='That\'s not your Pokemon!')
+    abort(400, message='Invalid Pokemon Data')
 
 # release/delete a pokemon
+  @jwt_required()
   def delete(self, pokemon_id):
-    request_data = request.get_json()
-    trainer_id = request_data['trainer_id']
+    trainer_id = get_jwt_identity()
     p = PokemonModel.query.get(pokemon_id)
     if p:
       if p.trainer_id == trainer_id:
